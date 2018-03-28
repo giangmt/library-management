217e60b50dba80fbe5e3328f3d75e7841afdc86e
giangmt <maitruonggiangbkhn@gmail.com>
2018-03-28 12:21:12 +0700

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-  	render html: "Welcome to Libray Management."
-  end
+  include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+
   root 'books#index'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   get 'static_pages/show'
   get 'static_pages/help'
   resources :books
